3461d38b2320efda22945a919e4454d79d99ea72
ohkyeongha <okh970526@gmail.com>
2021-07-26 13:22:31 +0900

    7/26 오전 commit

--- javascript_prj/WebContent/02_javascript/date.js
+++ javascript_prj/WebContent/02_javascript/date.js
@@ -1,3 +1,5 @@
 var nowDate = new Date();
-document.write(nowDate.getFullYear()+"년 "+(nowDate.getMonth()+1)+"월 "+nowDate.getDate()+"일 "
-				+ nowDate.getHours()+"시 " +nowDate.getMinutes()+"분 " + nowDate.getSeconds()+"초");
+document.write(nowDate.getFullYear() + "년 " + (nowDate.getMonth()+1) + "월 "
+				+ nowDate.getDate()+"일 " + nowDate.getHours() + "시 " 
+				+ nowDate.getMinutes() + "분 " + nowDate.getSeconds() + "초");
+document.getElementById("nowDate").innerHTML=day+time; setTimeout('nowDate()', 1000);
